ebd1372c541e9e7d84003d235a8d1fc81226248d
Prabal kanti khan <khanprabalkanti@gmail.com>
2025-06-22 16:47:28 +0530

    new app added

--- myapp/views.py
+++ myapp/views.py
@@ -4,3 +4,6 @@ def home(request):
     return render(request, 'index.html')
 def register(request):
     return render(request, 'register.html')
+def register(request):
+    return render(request, 'new/new.html')
+
